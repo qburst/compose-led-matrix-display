bd0c1ad74e2c7e11a56180ff9c8846143cb64380
Vishnu Haridas <vishnuh@qburst.com>
2021-09-07 00:30:15 +0530

    Improve the number '7': add the hook

--- app/src/main/java/com/qburst/compose/ledmatrix/composables/LedMatrixDisplay.kt
+++ app/src/main/java/com/qburst/compose/ledmatrix/composables/LedMatrixDisplay.kt
@@ -94,7 +94,7 @@ private val ribbon = listOf(
 
     // 7
     listOf(1, 1, 1, 1, 1), // index = 56
-    listOf(0, 0, 0, 0, 1),
+    listOf(1, 0, 0, 0, 1),
     listOf(0, 0, 0, 1, 0),
     listOf(0, 0, 1, 0, 0),
     listOf(0, 1, 0, 0, 0),
