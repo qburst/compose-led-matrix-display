b54a8658850b4802141eb6d1f436e606526d43fe
Vishnu Haridas <vishnuh@qburst.com>
2021-09-06 21:37:23 +0530

    Display a 3-digit counter to showcase the new Composable. Add two buttons, one to start/stop the counter, another one to randomize the counter.

--- app/src/main/java/com/qburst/compose/ledmatrix/MainActivity.kt
+++ app/src/main/java/com/qburst/compose/ledmatrix/MainActivity.kt
@@ -3,129 +3,16 @@ package com.qburst.compose.ledmatrix
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.qburst.compose.ledmatrix.composables.LedMatrixDisplay
 import com.qburst.compose.ledmatrix.ui.theme.LEDMatrixDisplayTheme
-
-val ribbon = listOf(
-
-    // 0
-    listOf(0, 1, 1, 1, 0), // index = 0
-    listOf(1, 0, 0, 0, 1),
-    listOf(1, 0, 0, 1, 1),
-    listOf(1, 0, 1, 0, 1),
-    listOf(1, 1, 0, 0, 1),
-    listOf(1, 0, 0, 0, 1),
-    listOf(0, 1, 1, 1, 0),
-
-    listOf(0, 0, 0, 0, 0), // Spacer
-
-    // 1
-    listOf(0, 0, 1, 0, 0), // index = 8
-    listOf(0, 1, 1, 0, 0),
-    listOf(0, 0, 1, 0, 0),
-    listOf(0, 0, 1, 0, 0),
-    listOf(0, 0, 1, 0, 0),
-    listOf(0, 0, 1, 0, 0),
-    listOf(0, 1, 1, 1, 0),
-
-    listOf(0, 0, 0, 0, 0), // Spacer
-
-    // 2
-    listOf(0, 1, 1, 1, 0),  // index = 16
-    listOf(1, 0, 0, 0, 1),
-    listOf(0, 0, 0, 0, 1),
-    listOf(0, 0, 0, 1, 0),
-    listOf(0, 0, 1, 0, 0),
-    listOf(0, 1, 0, 0, 0),
-    listOf(1, 1, 1, 1, 1),
-
-    listOf(0, 0, 0, 0, 0), // Spacer
-
-    // 3
-    listOf(0, 1, 1, 1, 0), // index = 24
-    listOf(1, 0, 0, 0, 1),
-    listOf(0, 0, 0, 0, 1),
-    listOf(0, 0, 1, 1, 0),
-    listOf(0, 0, 0, 0, 1),
-    listOf(1, 0, 0, 0, 1),
-    listOf(0, 1, 1, 1, 0),
-
-    listOf(0, 0, 0, 0, 0), // Spacer
-
-    // 4
-    listOf(0, 0, 0, 1, 0),  // index = 32
-    listOf(0, 0, 1, 1, 0),
-    listOf(0, 1, 0, 1, 0),
-    listOf(1, 0, 0, 1, 0),
-    listOf(1, 1, 1, 1, 1),
-    listOf(0, 0, 0, 1, 0),
-    listOf(0, 0, 0, 1, 0),
-
-    listOf(0, 0, 0, 0, 0), // Spacer
-
-    // 5
-    listOf(1, 1, 1, 1, 1), // index = 40
-    listOf(1, 0, 0, 0, 0),
-    listOf(1, 1, 1, 1, 0),
-    listOf(0, 0, 0, 0, 1),
-    listOf(0, 0, 0, 0, 1),
-    listOf(1, 0, 0, 0, 1),
-    listOf(0, 1, 1, 1, 0),
-
-    listOf(0, 0, 0, 0, 0), // Spacer
-
-    // 6
-    listOf(0, 1, 1, 1, 0), // index = 48
-    listOf(1, 0, 0, 0, 1),
-    listOf(1, 0, 0, 0, 0),
-    listOf(1, 1, 1, 1, 0),
-    listOf(1, 0, 0, 0, 1),
-    listOf(1, 0, 0, 0, 1),
-    listOf(0, 1, 1, 1, 0),
-
-    listOf(0, 0, 0, 0, 0), // Spacer
-
-    // 7
-    listOf(1, 1, 1, 1, 1), // index = 56
-    listOf(0, 0, 0, 0, 1),
-    listOf(0, 0, 0, 1, 0),
-    listOf(0, 0, 1, 0, 0),
-    listOf(0, 1, 0, 0, 0),
-    listOf(0, 1, 0, 0, 0),
-    listOf(0, 1, 0, 0, 0),
-
-    listOf(0, 0, 0, 0, 0), // Spacer
-
-    // 8
-    listOf(0, 1, 1, 1, 0), // index = 64
-    listOf(1, 0, 0, 0, 1),
-    listOf(1, 0, 0, 0, 1),
-    listOf(0, 1, 1, 1, 0),
-    listOf(1, 0, 0, 0, 1),
-    listOf(1, 0, 0, 0, 1),
-    listOf(0, 1, 1, 1, 0),
-
-    listOf(0, 0, 0, 0, 0), // Spacer
-
-    // 9
-    listOf(0, 1, 1, 1, 0), // index = 72
-    listOf(1, 0, 0, 0, 1),
-    listOf(1, 0, 0, 0, 1),
-    listOf(0, 1, 1, 1, 1),
-    listOf(0, 0, 0, 0, 1),
-    listOf(1, 0, 0, 0, 1),
-    listOf(0, 1, 1, 1, 0),
-)
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -133,80 +20,67 @@ class MainActivity : ComponentActivity() {
         setContent {
             LEDMatrixDisplayTheme {
 
-                Column {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
 
                     var number by remember { mutableStateOf(0) }
 
-                    LedMatrixDisplay(
-                        number = number
-                    )
+                    var started by remember { mutableStateOf(false) }
 
-                    repeat(10) {
+                    LaunchedEffect(started) {
+                        while (started) {
 
-                        Button(
-                            onClick = { number = it },
-                            modifier = Modifier.padding(4.dp)
-                        ) {
-                            Text("$it")
+                            number += 1
+
+                            if(number >= 1000){
+                                number = 0
+                            }
+
+                            delay(1000)
                         }
-
-
                     }
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+
+                        LedMatrixDisplay(number = number.div(100))
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        LedMatrixDisplay(number = number.mod(100).div(10))
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        LedMatrixDisplay(number = number % 10)
+                    }
+
+                    Button(
+                        onClick = { started = !started },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(
+                            if (started) "Stop" else "Start"
+                        )
+                    }
+
+                    Button(
+                        onClick = { number = (0..999).random() },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(
+                            "Randomize"
+                        )
+                    }
+
+
                 }
 
             }
         }
     }
-}
-
-@Composable
-fun LedMatrixDisplay(
-    number: Int = 0
-) {
-
-    val characterRow = when (number) {
-        in 0..9 -> number * 8
-        else -> 0
-    }
-
-    // This will "animate" an Integer to the new value, with the given `animationSpec`.
-    //
-    // In our case, imagine that the previous value was 0.
-    //
-    // If the new `targetValue` is 8, then it will update the value from 0 to 8, within the given
-    // `durationMillis`. On each update, the value will change from 0, 1, 2, 3... up to 8.
-    //
-    // On each update, the `LedMatrixDisplay` Composable will recompose (because of the "state"
-    // change), and the value of this state at that moment is used in the `background(..)`
-    // modifier of the `Box`.
-    val characterRowAnimated by animateIntAsState(
-        targetValue = characterRow,
-        animationSpec = tween(durationMillis = 500, easing = LinearEasing)
-    )
-
-    Column {
-
-        repeat(7) { row ->
-
-            Row {
-
-                repeat(5) { column ->
-
-                    Box(
-                        modifier = Modifier
-                            .padding(1.dp)
-                            .width(10.dp)
-                            .height(10.dp)
-                            .background(
-                                color = if (ribbon[characterRowAnimated + row][column] == 1) Color.Red else Color.LightGray
-                            )
-
-                    )
-                }
-
-            }
-
-        }
-    }
-
 }
