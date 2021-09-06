b6eefac47215311cabdfe9406b9c56ae9d70aa33
Vishnu Haridas <vishnuh@qburst.com>
2021-09-06 20:51:14 +0530

    Animate the LED Matrix display to scroll the characters vertically.

--- app/src/main/java/com/qburst/compose/ledmatrix/MainActivity.kt
+++ app/src/main/java/com/qburst/compose/ledmatrix/MainActivity.kt
@@ -3,6 +3,9 @@ package com.qburst.compose.ledmatrix
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
@@ -143,10 +146,25 @@ fun LedMatrixDisplay(
     number: Int = 0
 ) {
 
-    val characterRow = when(number){
+    val characterRow = when (number) {
         in 0..9 -> number * 7
         else -> 0
     }
+
+    // This will "animate" an Integer to the new value, with the given `animationSpec`.
+    //
+    // In our case, imagine that the previous value was 0.
+    //
+    // If the new `targetValue` is 8, then it will update the value from 0 to 8, within the given
+    // `durationMillis`. On each update, the value will change from 0, 1, 2, 3... up to 8.
+    //
+    // On each update, the `LedMatrixDisplay` Composable will recompose (because of the "state"
+    // change), and the value of this state at that moment is used in the `background(..)`
+    // modifier of the `Box`.
+    val characterRowAnimated by animateIntAsState(
+        targetValue = characterRow,
+        animationSpec = tween(durationMillis = 500, easing = LinearEasing)
+    )
 
     Column {
 
@@ -162,7 +180,7 @@ fun LedMatrixDisplay(
                             .width(10.dp)
                             .height(10.dp)
                             .background(
-                                color = if( ribbon[characterRow + row][column] == 1 ) Color.Red else Color.LightGray
+                                color = if (ribbon[characterRowAnimated + row][column] == 1) Color.Red else Color.LightGray
                             )
 
                     )
