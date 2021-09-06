eb5bc78ee9ca6d760ba7d934f20f86389da72ac0
Vishnu Haridas <vishnuh@qburst.com>
2021-09-06 22:07:44 +0530

    Extract the "Counter" setup into a separate Composable function.

--- app/src/main/java/com/qburst/compose/ledmatrix/MainActivity.kt
+++ app/src/main/java/com/qburst/compose/ledmatrix/MainActivity.kt
@@ -26,61 +26,66 @@ class MainActivity : ComponentActivity() {
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
 
-                    var number by remember { mutableStateOf(0) }
-
-                    var started by remember { mutableStateOf(false) }
-
-                    LaunchedEffect(started) {
-                        while (started) {
-
-                            number += 1
-
-                            if(number >= 1000){
-                                number = 0
-                            }
-
-                            delay(1000)
-                        }
-                    }
-
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-
-                        LedMatrixDisplay(number = number.div(100))
-
-                        Spacer(modifier = Modifier.width(8.dp))
-
-                        LedMatrixDisplay(number = number.mod(100).div(10))
-
-                        Spacer(modifier = Modifier.width(8.dp))
-
-                        LedMatrixDisplay(number = number % 10)
-                    }
-
-                    Button(
-                        onClick = { started = !started },
-                        modifier = Modifier.padding(8.dp)
-                    ) {
-                        Text(
-                            if (started) "Stop" else "Start"
-                        )
-                    }
-
-                    Button(
-                        onClick = { number = (0..999).random() },
-                        modifier = Modifier.padding(8.dp)
-                    ) {
-                        Text(
-                            "Randomize"
-                        )
-                    }
-
+                    LedCounterDisplay()
 
                 }
 
             }
         }
+    }
+}
+
+@Composable
+private fun LedCounterDisplay() {
+
+    var number by remember { mutableStateOf(0) }
+
+    var started by remember { mutableStateOf(false) }
+
+    LaunchedEffect(started) {
+        while (started) {
+
+            number += 1
+
+            if (number >= 1000) {
+                number = 0
+            }
+
+            delay(1000)
+        }
+    }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+
+        LedMatrixDisplay(number = number.div(100))
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        LedMatrixDisplay(number = number.mod(100).div(10))
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        LedMatrixDisplay(number = number % 10)
+    }
+
+    Button(
+        onClick = { started = !started },
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Text(
+            if (started) "Stop" else "Start"
+        )
+    }
+
+    Button(
+        onClick = { number = (0..999).random() },
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Text(
+            "Randomize"
+        )
     }
 }
